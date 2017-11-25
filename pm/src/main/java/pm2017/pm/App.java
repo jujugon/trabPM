70053a1bf2762f00e6089a546122ce49c1339d7e
lucasrb <lucas_tarof@hotmail.com>
2017-11-25 18:02:55 -0200

    Projeto criado com maven

---
+++ pm/src/main/java/pm2017/pm/App.java
@@ -0,0 +1,13 @@
+package pm2017.pm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
